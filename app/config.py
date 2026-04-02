e746cd3d10324954423e8afcf2233ae844d2fb12
Dhiraj Kumar <gdhiraj499@gmail.com>
2026-04-03 02:05:05 +0530

    chore: add centralised configuration for app and services

--- app/config.py
+++ app/config.py
@@ -6,3 +6,6 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 QDRANT_URL= os.getenv("QDRANT_URL")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+
+REDIS_HOST= os.getenv("REDIS_HOST")
+REDIS_PORT= int(os.getenv("REDIS_PORT",6379))
