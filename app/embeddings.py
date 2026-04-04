203b3133a009a50093d77da0269fa1a6decb68f7
Dhiraj Kumar <gdhiraj499@gmail.com>
2026-04-04 20:20:53 +0530

    feat: add async RAG pipeline with caching support

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,11 +1,18 @@
-from openai import OpenAI
+import asyncio
+from openai import AsyncOpenAI
 from app.config import OPENAI_API_KEY
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = AsyncOpenAI(api_key=OPENAI_API_KEY,timeout=10.0)
 
-def get_embedding(text:str):
-    response = client.embeddings.create(
-        model="text-embedding-3-small",
-        input=text
-    )
-    return response.data[0].embedding
+async def get_embedding(text:str):
+    for _ in range(3):
+        try:
+
+            response = await client.embeddings.create(
+                model="text-embedding-3-small",
+                input=text
+            )
+            return response.data[0].embedding
+        except Exception as e:
+            await asyncio.sleep(1)
+    raise Exception("OpenAI API failed after retries")
